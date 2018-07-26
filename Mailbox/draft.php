63841581bc3f591c2c39869dccd9b5ac3f4cc903
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-07-26 11:47:56 +0530

    Draft !

---
+++ Mailbox/draft.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+$uname=$_SESSION['username'];
+include("connect.php");
+$res=mysql_query("select * from draft where Sender='$uname'");
+?>
+<br />
+<table border="2" align="center" cellpadding="10" cellspacing="5">
+<tr>
+<th>From</th>
+<th>To</th>
+<th>Subject</th>
+<th>Message</th>
+<th>Date</th>
+</tr>
+<?php
+ while($row=mysql_fetch_array($res))
+{
+ echo "<tr><td>$row[1]</td><td>$row[2]</td><td>$row[3]</td><td>$row[4]</td><td>$row[5]</td></tr>";
+}
+echo "</table>";
+?>
