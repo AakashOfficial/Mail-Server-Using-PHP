592f3a12a6c7c5cc71a6447aee865661408a0158
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-07-26 11:44:43 +0530

    Compose !

---
+++ Mailbox/compose.php
@@ -0,0 +1,28 @@
+<html>
+<head>
+<title>
+MAIL FORM
+</title>
+</head>
+<body>
+<form action="" method="post">
+<fieldset title="40" style="border-width:thick">
+<h1>To:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<select id="user" name="to"  style="width:320">
+<option selected="selected" value="-1">Select a Receiver</option>
+</select>
+<br>
+<h1>Subject:
+<input type="text" size="50" name="sub">
+<br>
+<h1>Msg:<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<textarea name="msg" cols="15" rows="8"></textarea><br><br>
+</h1>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type="submit" name="send" value="SEND" style="background-color:#6633CC">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<input type="submit" name="draft" value="SAVE TO DRAFT" style="background-color:#6633CC">
+
+</fieldset>
+</form>
+
+</body>
+</html>
